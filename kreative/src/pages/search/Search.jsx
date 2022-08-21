9ad6a23ee8ddfc26387deb94096f910cf93c3c2b
Aliaksandr Kot <alven81.ak@gmail.com>
2022-08-21 12:35:20 +0200

    update search component

--- kreative/src/pages/search/Search.jsx
+++ kreative/src/pages/search/Search.jsx
@@ -3,23 +3,32 @@ import CatElement from "../catalog/CatElement";
 import { New } from "../product/New";
 
 const Search = () => {
-	const searchCatalog = useSelector((state) => state.loadSearch.searchResult);
+    const searchCatalog = useSelector((state) => state.loadSearch.searchResult);
 
-	return (
-		<>
-			<div className="catalog_main container">
-				{searchCatalog && (
-					<>
-						{searchCatalog.map((catalog) => (
-							<CatElement key={catalog.id} catalog={catalog} />
-						))}
-					</>
-				)}
-			</div>
-			<div className="container">
-				<New />
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className="catalog_main container">
+                {searchCatalog && (
+                    <>
+                        {searchCatalog.length > 0 ? (
+                            searchCatalog.map((catalog) => (
+                                <CatElement
+                                    key={catalog.id}
+                                    catalog={catalog}
+                                />
+                            ))
+                        ) : (
+                            <div className="catalog_main-search">
+                                <p>No search result!</p>
+                            </div>
+                        )}
+                    </>
+                )}
+            </div>
+            <div className="container">
+                <New />
+            </div>
+        </>
+    );
 };
 export default Search;
